3d5df7485cace953b2408ec8c016b0d1daad26ca
AntonFio <anton_fiolkovski@mail.ru>
2022-10-15 16:10:48 +0300

    feat: error

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,11 +12,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ class DepthCalculator {
+	calculateDepth(arr) {
+	return arr.reduce((count, i) => !Array.isArray(i) ? count : 1 + this.calculateDepth(i), 1)
+}
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function transform(arr) {
+	if (arr === null) return "'arr' parameter must be an instance of the Array!";
+	if (arr === undefined) return "'arr' parameter must be an instance of the Array!";
+	if (arr.length === 0) return [];
+	if (Array.isArray(arr) !== 'object') return "'arr' parameter must be an instance of the Array!";
+	if(typeof arr ===  'number' ||typeof arr ===  false) return "'arr' parameter must be an instance of the Array!"
+	let arr2 =[]
+	for (let i = 0; i < arr.length; i++){
+		if (arr[i] === '--discard-next') {
+			delete arr[i + 1]
+		} else if (arr[i] === '--discard-prev') {
+			delete arr2[i-1]
+		}else if (arr[i] === '--double-next') {
+			arr2.push(arr[i + 1])
+		}else if (arr[i] === '--double-prev') {
+			arr2.push(arr[i-1])
+		} else {
+			arr2.push(arr[i])
+		}
+	}
+
+return arr2.filter(item => item >0)
+ }
+
 
 module.exports = {
   transform
