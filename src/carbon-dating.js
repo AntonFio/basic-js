cbd566b2ef8fbaa521325d2f3d13217600c22de4
AntonFio <anton_fiolkovski@mail.ru>
2022-10-17 01:41:32 +0300

    feat: carbone new

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -20,7 +20,8 @@ const HALF_LIFE_PERIOD = 5730;
  function dateSample(sampleActivity) {
   if (typeof sampleActivity !== 'string') return false;
   if (+(sampleActivity) == NaN) return false;
-  if (+(sampleActivity) > 15) return false;
+   if (+(sampleActivity) > 15) return false;
+   if (isNaN(+(sampleActivity))) return false;
    if (+(sampleActivity) <= 0) return false;
   return Math.ceil(Math.log(MODERN_ACTIVITY / +(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
 }
