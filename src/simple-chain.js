138373141157015e65e5ca4991920794e650077d
AntonFio <anton_fiolkovski@mail.ru>
2022-10-16 17:04:33 +0300

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  startChain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+     return this.startChain.length
+     
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+     this.startChain.push(`( ${ value } )`)
+     return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+     if (typeof(position-1) !== 'number'|| !this.startChain[position - 1]) {
+        this.startChain = []
+        throw new Error("You can't remove incorrect link!");
+     }else {
+        this.startChain.splice(position - 1, 1)
+        return this
+     }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+     this.startChain.reverse()
+     return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+     let result = this.startChain.join('~~');
+     this.startChain = [];
+     return result
   }
 };
 
